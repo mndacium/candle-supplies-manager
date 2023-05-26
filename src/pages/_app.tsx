bf5d505bafa6bb71da3a83ca5668dd376dadef3d
Vladislav Tanasiichuk <vladoscompany@gmail.com>
2023-05-26 08:52:01 +0300

    Added font for deployment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,25 @@
 import "@/styles/globals.css";
-import type { ReactElement, ReactNode } from 'react'
-import type { NextPage } from 'next'
-import type { AppProps } from 'next/app'
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({
+  weight: "400",
+  subsets: ["latin"],
+  variable: '--font-montserrat'
+});
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode
-}
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout
-}
+  Component: NextPageWithLayout;
+};
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-
-  const getLayout = Component.getLayout ?? ((page) => page)
-
-  return getLayout(<Component {...pageProps} />)
+  const getLayout = Component.getLayout ?? ((page) => page);
+<main className={`${montserrat.variable} font-sans`}></main>
+  return getLayout(<Component {...pageProps} />);
 }
